517c9106de1b1f4885e1c79fc8780e1b107e7c2a
jtabach <jeff.n.tabachnick@gmail.com>
2016-01-23 18:22:39 -0800

    nice css UI/UX

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
 $(document).ready(function() {
-	$('button').click(convertText);
+	$('textarea').keyup(function() {
+		if (this.timeoutId)
+			window.clearTimeout(this.timeoutId);
+		this.timeoutId = window.setTimeout(function () {
+			var input = $('.input').val();
+			$.post('./converts', {'text': input}, function(data) {
+				var parsed = $.parseHTML(data);
+				$('.output').html(parsed);
+			});
+		}, 100);
+	});
 });
-
-function convertText() {
-	 var input = $('.input').val();
-	 $.post('./converts', {'text': input}, function(data) {
-	 	var parsed = $.parseHTML(data);
-	 	$('.output').html(parsed);
-	 });
-}
